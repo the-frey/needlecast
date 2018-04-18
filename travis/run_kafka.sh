790cc1d0945bccbbb2e0de19de3c2fe4e06c9d8e
Alex Lynham <acelynham@hotmail.com>
2018-04-18 23:35:14 +0100

    Force zookeeper into the bg.

--- travis/run_kafka.sh
+++ travis/run_kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ./kafka/kafka_2.11-1.1.0
-sudo bin/zookeeper-server-start.sh config/zookeeper.properties &
+sudo nohup bin/zookeeper-server-start.sh config/zookeeper.properties > /dev/null 2>&1 &
 sudo bin/kafka-server-start.sh -daemon config/server.properties &
