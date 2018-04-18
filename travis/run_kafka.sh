5b15c3f9a737a1fb46b64fb27fc5ed2f5e10daa7
Alex Lynham <acelynham@hotmail.com>
2018-04-18 23:28:11 +0100

    Run as daemon in kafkafile to prevent travis shell locking up.

--- travis/run_kafka.sh
+++ travis/run_kafka.sh
@@ -2,4 +2,4 @@
 
 cd ./kafka/kafka_2.11-1.1.0
 sudo bin/zookeeper-server-start.sh config/zookeeper.properties &
-sudo bin/kafka-server-start.sh config/server.properties &
+sudo bin/kafka-server-start.sh -daemon config/server.properties &
